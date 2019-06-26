468e05ff4a3a7e994b3131d9436f2d5bbcd57104
rajenshrestha@hotmail.com <rajen.shrestha@icloud.com>
2019-06-27 01:25:14 +1000

    add validation

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,41 +1,100 @@
-import { validate, IsInt, Length, IsEmail, IsDate, Min, Max } from 'class-validator';
+import { IValidator, ValidationResult, Validator } from 'ts.validator.fluent/dist';
 
 export class Employee {
-    @Length(3, 10)
     title: string;
-
     name: string;
     lastName: string;
     middleName: string;
-
-    @IsEmail()
     email!: string;
-    @IsDate()
     joinDate: Date = new Date();
-
-    @IsInt()
-    @Min(8)
-    @Max(10)
     phone!: number;
-
-    @IsInt()
-    @Min(8)
-    @Max(10)
     mobile_phone!: number;
 
-    constructor(name: string, lastName: string) {
+    constructor() {
         this.title = "Mr.";
-        this.name = name;
+        this.name = "";
         this.middleName = "";
-        this.lastName = lastName;
+        this.lastName = "";
         this.joinDate = new Date();
-    }
-
-    validation() {
-        return validate(this);
     }
 
     fullName() {
         return `${this.name} ${this.lastName}`;
     }
+
+    private validateNullValue(validator: IValidator<Employee>): ValidationResult {
+        let nullMessage = "Should not be null";
+        return validator
+            .NotNull(m => m.title, nullMessage, "Employee.name.Null")
+            .NotNull(m => m.lastName, nullMessage, "Employee.lastName.Null")
+            .NotNull(m => m.name, nullMessage, "Employee.name.Null")
+            .NotNull(m => m.email, nullMessage, "Employee.email.null").ToResult();
+    }
+
+
+    private validateEmptyValue(validator: IValidator<Employee>): ValidationResult {
+        let emptyMessage = "Should not empty";
+        return validator
+            .NotEmpty(m => m.title, emptyMessage, "Emplyee.name.Empty")
+            .NotEmpty(m => m.name, emptyMessage, "Employee.name.Empty")
+            .NotEmpty(m => m.lastName, emptyMessage, "Employee.lastName.Empty")
+            .NotEmpty(m => m.name, emptyMessage, "Employee.name.Empty")
+            .NotEmpty(m => m.email, emptyMessage, "Employee.email.Empty").ToResult();
+    }
+
+    private validateIsEmail(validator: IValidator<Employee>): ValidationResult {
+
+        return validator.Email(x => x.email, "Not a valid email", "Employee.email.Not_Valid").ToResult();
+    }
+
+    private validatePhone(validator: IValidator<Employee>): ValidationResult {
+
+        return validator
+            .If(x => x.phone === undefined,
+                validator => validator
+                    .NotNull(x => x.mobile_phone, "Mobile number cannot be null", "Employee.Mobile.IsNull")
+                    .If(x => x.mobile_phone !== undefined,
+                        validator => validator.Matches(x => x.mobile_phone.toString(), "^[0-9]{8,10}$", "Not a valid number", "Employee.mobile_phone.Not_Valid").ToResult())
+                    .ToResult())
+            .If(x => x.mobile_phone === undefined,
+                validator => validator
+                    .NotNull(x => x.phone, "Mobile or phone is required", "Employee.phone.IsNull")
+                    .If(x => x.phone !== undefined,
+                        validator => validator.Matches(x => x.phone.toString(), "^[0-9]{8,10}$", "Not a valid number", "Employee.phone.Not_Valid").ToResult())
+                    .ToResult())
+            .ToResult();
+    }
+
+    validate(): string {
+        let error: string = '';
+        let resultNullValue = new Validator(this).Validate(this.validateNullValue);
+        let resultEmptyValue = new Validator(this).Validate(this.validateEmptyValue);
+        let resultEmailValue = new Validator(this).Validate(this.validateIsEmail);
+        let resultPhoneValue = new Validator(this).Validate(this.validatePhone);
+
+        if (!resultNullValue.IsValid
+            || !resultEmptyValue.IsValid
+            || !resultEmailValue.IsValid
+            || !resultPhoneValue.IsValid) {
+
+            resultNullValue.Errors.forEach(x =>
+                error += `Error: ${x.Identifier}-${x.Value}: ${x.Message} \n`
+            );
+
+            resultEmptyValue.Errors.forEach(x =>
+                error += `Error: ${x.Identifier}-${x.Value}: ${x.Message} \n`
+            );
+
+            resultEmailValue.Errors.forEach(x =>
+                error += `Error: ${x.Identifier}-${x.Value}: ${x.Message} \n`
+            );
+
+            resultPhoneValue.Errors.forEach(x =>
+                error += `Error: ${x.Identifier}-${x.Value}: ${x.Message} \n`
+            );
+        }
+        return error;
+    }
 }
+//https://www.npmjs.com/package/ts.validator.fluent
+//https://www.c-sharpcorner.com/article/ts-validator-typescript-based-generic-validation-framework/
